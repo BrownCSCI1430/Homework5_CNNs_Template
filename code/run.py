13fc19ce7c3a5c7f61e55797c241b010a4856816
Arvind Yalavarti <yalavartiarvind@gmail.com>
2021-03-02 12:09:30 -0500

    Update LIME and argparse

--- code/run.py
+++ code/run.py
@@ -14,18 +14,18 @@ import tensorflow as tf
 import hyperparameters as hp
 from models import YourModel, VGGModel
 from preprocess import Datasets
+from skimage.transform import resize
 from tensorboard_utils import \
         ImageLabelingLogger, ConfusionMatrixLogger, CustomModelSaver
 
 from skimage.io import imread
-from skimage.color import rgb2grey
-import lime
 from lime import lime_image
 from skimage.segmentation import mark_boundaries
 from matplotlib import pyplot as plt
 import numpy as np
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 
 def parse_args():
     """ Perform command-line argument parsing. """
@@ -41,7 +41,7 @@ def parse_args():
         training from scratch (1), or fine tuning VGG-16 (2).''')
     parser.add_argument(
         '--data',
-        default='..'+'os.sep'+'data'+os.sep,
+        default='../'+os.sep+'data'+os.sep,
         help='Location where the dataset is stored.')
     parser.add_argument(
         '--load-vgg',
@@ -67,46 +67,60 @@ def parse_args():
         help='''Skips training and evaluates on the test set once.
         You can use this to test an already trained model by loading
         its checkpoint.''')
+    parser.add_argument(
+        '--lime-image',
+        default='test/Bedroom/image_0003',
+        help='''Name of an image in the dataset to use for LIME evaluation.''')
 
     return parser.parse_args()
 
 
-def LIME_explainer(model, image):
-    '''
-    This function takes in an image and outputs 5 visual explanations using the 
-    LIME model
-    '''
-    explainer = lime_image.LimeImageExplainer()
+def LIME_explainer(model, path, preprocess_fn):
+    """
+    This function takes in a trained model and a path to an image and outputs 5
+    visual explanations using the LIME model
+    """
 
-    # TODO: change the image path to be the image of your choice, test images can be found in the data folder
-    path = '../data/test/Bedroom/image_0003.jpg'
+    def image_and_mask(title, positive_only=True, num_features=5,
+                       hide_rest=True):
+        temp, mask = explanation.get_image_and_mask(
+            explanation.top_labels[0], positive_only=positive_only,
+            num_features=num_features, hide_rest=hide_rest)
+        plt.imshow(mark_boundaries(temp / 2 + 0.5, mask))
+        plt.title(title)
+        plt.show()
 
     image = imread(path)
-    image = np.resize(image, (224, 224, 3))
-    # image = image[0][0][0, :, :, :]
-    explanation = explainer.explain_instance(image.astype('double'), model.predict, top_labels=5, hide_color=0, num_samples=1000)
+    if len(image.shape) == 2:
+        image = np.stack([image, image, image], axis=-1)
+    image = preprocess_fn(image)
+    image = resize(image, (hp.img_size, hp.img_size, 3))
 
-    #the top 5 superpixels that are most positive towards the class with the rest of the image hidden
-    temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=5, hide_rest=True)
-    plt.imshow(mark_boundaries(temp / 2 + 0.5, mask))
-    plt.title("Top 5 superpixels")
-    plt.show()
-    #the top 5 superpixels with the rest of the image present
-    temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=5, hide_rest=False)
-    plt.imshow(mark_boundaries(temp / 2 + 0.5, mask))
-    plt.title("Top 5 superpixels with the rest of the image present")
-    plt.show()
-    # the 'pros and cons' (pros in green, cons in red)
-    temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=False, num_features=10, hide_rest=False)
-    plt.imshow(mark_boundaries(temp / 2 + 0.5, mask))
-    plt.title("Pros(green) and Cons(red)")
-    #Select the same class explained on the figures above.
-    ind =  explanation.top_labels[0]
-    plt.show()
-    #Map each explanation weight to the corresponding superpixel
+    explainer = lime_image.LimeImageExplainer()
+
+    explanation = explainer.explain_instance(
+        image.astype('double'), model.predict, top_labels=5, hide_color=0,
+        num_samples=1000)
+
+    # The top 5 superpixels that are most positive towards the class with the
+    # rest of the image hidden
+    image_and_mask("Top 5 superpixels", positive_only=True, num_features=5,
+                   hide_rest=True)
+
+    # The top 5 superpixels with the rest of the image present
+    image_and_mask("Top 5 with the rest of the image present",
+                   positive_only=True, num_features=5, hide_rest=False)
+
+    # The 'pros and cons' (pros in green, cons in red)
+    image_and_mask("Pros(green) and Cons(red)",
+                   positive_only=False, num_features=10, hide_rest=False)
+
+    # Select the same class explained on the figures above.
+    ind = explanation.top_labels[0]
+    # Map each explanation weight to the corresponding superpixel
     dict_heatmap = dict(explanation.local_exp[ind])
-    heatmap = np.vectorize(dict_heatmap.get)(explanation.segments) 
-    plt.imshow(heatmap, cmap = 'RdBu', vmin  = -heatmap.max(), vmax = heatmap.max())
+    heatmap = np.vectorize(dict_heatmap.get)(explanation.segments)
+    plt.imshow(heatmap, cmap='RdBu', vmin=-heatmap.max(), vmax=heatmap.max())
     plt.colorbar()
     plt.title("Map each explanation weight to the corresponding superpixel")
     plt.show()
@@ -223,13 +237,13 @@ def main():
         metrics=["sparse_categorical_accuracy"])
 
     if ARGS.evaluate:
-        # path = '../data/test/Bedroom/image_0004.jpg'
-
-        # image = imread(path)
-        # print(image.shape)
-        # image = np.resize(image, (224, 224, 3))
         test(model, datasets.test_data)
-        LIME_explainer(model, datasets.test_data)
+
+        # TODO: change the image path to be the image of your choice by changing
+        # the lime-image flag when calling run.py to investigate
+        # i.e. python run.py --evaluate --lime-image test/Bedroom/image_003.jpg
+        path = ARGS.data + os.sep + ARGS.lime_image
+        LIME_explainer(model, path, datasets.preprocess_fn)
     else:
         train(model, datasets, checkpoint_path, logs_path, init_epoch)
 
